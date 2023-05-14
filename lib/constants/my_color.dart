4c1e3362b69f17a348d2c4d493a9c6eb174e92bf
on-stu <minsu0523@naver.com>
2023-05-14 15:33:42 +0900

    FEAT : add tailwind css colors

--- lib/constants/my_color.dart
+++ lib/constants/my_color.dart
@@ -1,1 +1,292 @@
-class MyColor {}
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+
+  // slate
+  static const slate_50 = Color.fromRGBO(248, 250, 252, 1);
+  static const slate_100 = Color.fromRGBO(241, 245, 249, 1);
+  static const slate_200 = Color.fromRGBO(226, 232, 240, 1);
+  static const slate_300 = Color.fromRGBO(203, 213, 225, 1);
+  static const slate_400 = Color.fromRGBO(148, 163, 184, 1);
+  static const slate_500 = Color.fromRGBO(100, 116, 139, 1);
+  static const slate_600 = Color.fromRGBO(71, 85, 105, 1);
+  static const slate_700 = Color.fromRGBO(51, 65, 85, 1);
+  static const slate_800 = Color.fromRGBO(30, 41, 59, 1);
+  static const slate_900 = Color.fromRGBO(15, 23, 42, 1);
+  static const slate_950 = Color.fromRGBO(2, 6, 23, 1);
+
+  // gray
+  static const gray_50 = Color.fromRGBO(249, 250, 251, 1);
+  static const gray_100 = Color.fromRGBO(243, 244, 246, 1);
+  static const gray_200 = Color.fromRGBO(229, 231, 235, 1);
+  static const gray_300 = Color.fromRGBO(209, 213, 219, 1);
+  static const gray_400 = Color.fromRGBO(156, 163, 175, 1);
+  static const gray_500 = Color.fromRGBO(107, 114, 128, 1);
+  static const gray_600 = Color.fromRGBO(75, 85, 99, 1);
+  static const gray_700 = Color.fromRGBO(55, 65, 81, 1);
+  static const gray_800 = Color.fromRGBO(31, 41, 55, 1);
+  static const gray_900 = Color.fromRGBO(17, 24, 39, 1);
+  static const gray_950 = Color.fromRGBO(3, 7, 18, 1);
+
+  // zinc
+  static const zinc_50 = Color.fromRGBO(250, 250, 250, 1);
+  static const zinc_100 = Color.fromRGBO(244, 244, 245, 1);
+  static const zinc_200 = Color.fromRGBO(228, 228, 231, 1);
+  static const zinc_300 = Color.fromRGBO(212, 212, 216, 1);
+  static const zinc_400 = Color.fromRGBO(161, 161, 170, 1);
+  static const zinc_500 = Color.fromRGBO(113, 113, 122, 1);
+  static const zinc_600 = Color.fromRGBO(82, 82, 91, 1);
+  static const zinc_700 = Color.fromRGBO(63, 63, 70, 1);
+  static const zinc_800 = Color.fromRGBO(39, 39, 42, 1);
+  static const zinc_900 = Color.fromRGBO(24, 24, 27, 1);
+  static const zinc_950 = Color.fromRGBO(9, 9, 11, 1);
+
+  // neutral
+  static const neutral_50 = Color.fromRGBO(250, 250, 250, 1);
+  static const neutral_100 = Color.fromRGBO(245, 245, 245, 1);
+  static const neutral_200 = Color.fromRGBO(229, 229, 229, 1);
+  static const neutral_300 = Color.fromRGBO(212, 212, 212, 1);
+  static const neutral_400 = Color.fromRGBO(163, 163, 163, 1);
+  static const neutral_500 = Color.fromRGBO(115, 115, 115, 1);
+  static const neutral_600 = Color.fromRGBO(82, 82, 82, 1);
+  static const neutral_700 = Color.fromRGBO(64, 64, 64, 1);
+  static const neutral_800 = Color.fromRGBO(38, 38, 38, 1);
+  static const neutral_900 = Color.fromRGBO(23, 23, 23, 1);
+  static const neutral_950 = Color.fromRGBO(10, 10, 10, 1);
+
+  // stone
+  static const stone_50 = Color.fromRGBO(250, 250, 249, 1);
+  static const stone_100 = Color.fromRGBO(245, 245, 244, 1);
+  static const stone_200 = Color.fromRGBO(231, 229, 228, 1);
+  static const stone_300 = Color.fromRGBO(214, 211, 209, 1);
+  static const stone_400 = Color.fromRGBO(168, 162, 158, 1);
+  static const stone_500 = Color.fromRGBO(120, 113, 108, 1);
+  static const stone_600 = Color.fromRGBO(87, 83, 78, 1);
+  static const stone_700 = Color.fromRGBO(68, 64, 60, 1);
+  static const stone_800 = Color.fromRGBO(41, 37, 36, 1);
+  static const stone_900 = Color.fromRGBO(28, 25, 23, 1);
+  static const stone_950 = Color.fromRGBO(12, 10, 9, 1);
+
+  // red
+  static const red_50 = Color.fromRGBO(254, 242, 242, 1);
+  static const red_100 = Color.fromRGBO(254, 226, 226, 1);
+  static const red_200 = Color.fromRGBO(254, 202, 202, 1);
+  static const red_300 = Color.fromRGBO(252, 165, 165, 1);
+  static const red_400 = Color.fromRGBO(248, 113, 113, 1);
+  static const red_500 = Color.fromRGBO(239, 68, 68, 1);
+  static const red_600 = Color.fromRGBO(220, 38, 38, 1);
+  static const red_700 = Color.fromRGBO(185, 28, 28, 1);
+  static const red_800 = Color.fromRGBO(153, 27, 27, 1);
+  static const red_900 = Color.fromRGBO(127, 29, 29, 1);
+  static const red_950 = Color.fromRGBO(69, 10, 10, 1);
+
+  // orange
+  static const orange_50 = Color.fromRGBO(255, 247, 237, 1);
+  static const orange_100 = Color.fromRGBO(255, 237, 213, 1);
+  static const orange_200 = Color.fromRGBO(254, 215, 170, 1);
+  static const orange_300 = Color.fromRGBO(253, 186, 116, 1);
+  static const orange_400 = Color.fromRGBO(251, 146, 60, 1);
+  static const orange_500 = Color.fromRGBO(249, 115, 22, 1);
+  static const orange_600 = Color.fromRGBO(234, 88, 12, 1);
+  static const orange_700 = Color.fromRGBO(194, 65, 12, 1);
+  static const orange_800 = Color.fromRGBO(154, 52, 18, 1);
+  static const orange_900 = Color.fromRGBO(124, 45, 18, 1);
+  static const orange_950 = Color.fromRGBO(67, 20, 7, 1);
+
+  // amber
+  static const amber_50 = Color.fromRGBO(255, 251, 235, 1);
+  static const amber_100 = Color.fromRGBO(254, 243, 199, 1);
+  static const amber_200 = Color.fromRGBO(253, 230, 138, 1);
+  static const amber_300 = Color.fromRGBO(252, 211, 77, 1);
+  static const amber_400 = Color.fromRGBO(251, 191, 36, 1);
+  static const amber_500 = Color.fromRGBO(245, 158, 11, 1);
+  static const amber_600 = Color.fromRGBO(217, 119, 6, 1);
+  static const amber_700 = Color.fromRGBO(180, 83, 9, 1);
+  static const amber_800 = Color.fromRGBO(146, 64, 14, 1);
+  static const amber_900 = Color.fromRGBO(120, 53, 15, 1);
+  static const amber_950 = Color.fromRGBO(69, 26, 3, 1);
+
+  // yellow
+  static const yellow_50 = Color.fromRGBO(254, 252, 232, 1);
+  static const yellow_100 = Color.fromRGBO(254, 249, 195, 1);
+  static const yellow_200 = Color.fromRGBO(254, 240, 138, 1);
+  static const yellow_300 = Color.fromRGBO(253, 224, 71, 1);
+  static const yellow_400 = Color.fromRGBO(250, 204, 21, 1);
+  static const yellow_500 = Color.fromRGBO(234, 179, 8, 1);
+  static const yellow_600 = Color.fromRGBO(202, 138, 4, 1);
+  static const yellow_700 = Color.fromRGBO(161, 98, 7, 1);
+  static const yellow_800 = Color.fromRGBO(133, 77, 14, 1);
+  static const yellow_900 = Color.fromRGBO(113, 63, 18, 1);
+  static const yellow_950 = Color.fromRGBO(66, 32, 6, 1);
+
+  // lime
+  static const lime_50 = Color.fromRGBO(247, 254, 231, 1);
+  static const lime_100 = Color.fromRGBO(236, 252, 203, 1);
+  static const lime_200 = Color.fromRGBO(217, 249, 157, 1);
+  static const lime_300 = Color.fromRGBO(190, 242, 100, 1);
+  static const lime_400 = Color.fromRGBO(163, 230, 53, 1);
+  static const lime_500 = Color.fromRGBO(132, 204, 22, 1);
+  static const lime_600 = Color.fromRGBO(101, 163, 13, 1);
+  static const lime_700 = Color.fromRGBO(77, 124, 15, 1);
+  static const lime_800 = Color.fromRGBO(63, 98, 18, 1);
+  static const lime_900 = Color.fromRGBO(54, 83, 20, 1);
+  static const lime_950 = Color.fromRGBO(26, 46, 5, 1);
+
+  // green
+  static const green_50 = Color.fromRGBO(240, 253, 244, 1);
+  static const green_100 = Color.fromRGBO(220, 252, 231, 1);
+  static const green_200 = Color.fromRGBO(187, 247, 208, 1);
+  static const green_300 = Color.fromRGBO(134, 239, 172, 1);
+  static const green_400 = Color.fromRGBO(74, 222, 128, 1);
+  static const green_500 = Color.fromRGBO(34, 197, 94, 1);
+  static const green_600 = Color.fromRGBO(22, 163, 74, 1);
+  static const green_700 = Color.fromRGBO(21, 128, 61, 1);
+  static const green_800 = Color.fromRGBO(22, 101, 52, 1);
+  static const green_900 = Color.fromRGBO(20, 83, 45, 1);
+  static const green_950 = Color.fromRGBO(5, 46, 22, 1);
+
+  // emerald
+  static const emerald_50 = Color.fromRGBO(236, 253, 245, 1);
+  static const emerald_100 = Color.fromRGBO(209, 250, 229, 1);
+  static const emerald_200 = Color.fromRGBO(167, 243, 208, 1);
+  static const emerald_300 = Color.fromRGBO(110, 231, 183, 1);
+  static const emerald_400 = Color.fromRGBO(52, 211, 153, 1);
+  static const emerald_500 = Color.fromRGBO(16, 185, 129, 1);
+  static const emerald_600 = Color.fromRGBO(5, 150, 105, 1);
+  static const emerald_700 = Color.fromRGBO(4, 120, 87, 1);
+  static const emerald_800 = Color.fromRGBO(6, 95, 70, 1);
+  static const emerald_900 = Color.fromRGBO(6, 78, 59, 1);
+  static const emerald_950 = Color.fromRGBO(2, 44, 34, 1);
+
+  // teal
+  static const teal_50 = Color.fromRGBO(240, 253, 250, 1);
+  static const teal_100 = Color.fromRGBO(204, 251, 241, 1);
+  static const teal_200 = Color.fromRGBO(153, 246, 228, 1);
+  static const teal_300 = Color.fromRGBO(94, 234, 212, 1);
+  static const teal_400 = Color.fromRGBO(45, 212, 191, 1);
+  static const teal_500 = Color.fromRGBO(20, 184, 166, 1);
+  static const teal_600 = Color.fromRGBO(13, 148, 136, 1);
+  static const teal_700 = Color.fromRGBO(15, 118, 110, 1);
+  static const teal_800 = Color.fromRGBO(17, 94, 89, 1);
+  static const teal_900 = Color.fromRGBO(19, 78, 74, 1);
+  static const teal_950 = Color.fromRGBO(4, 47, 46, 1);
+
+  // cyan
+  static const cyan_50 = Color.fromRGBO(236, 254, 255, 1);
+  static const cyan_100 = Color.fromRGBO(207, 250, 254, 1);
+  static const cyan_200 = Color.fromRGBO(165, 243, 252, 1);
+  static const cyan_300 = Color.fromRGBO(103, 232, 249, 1);
+  static const cyan_400 = Color.fromRGBO(34, 211, 238, 1);
+  static const cyan_500 = Color.fromRGBO(6, 182, 212, 1);
+  static const cyan_600 = Color.fromRGBO(8, 145, 178, 1);
+  static const cyan_700 = Color.fromRGBO(14, 116, 144, 1);
+  static const cyan_800 = Color.fromRGBO(21, 94, 117, 1);
+  static const cyan_900 = Color.fromRGBO(22, 78, 99, 1);
+  static const cyan_950 = Color.fromRGBO(8, 51, 68, 1);
+
+  // sky
+  static const sky_50 = Color.fromRGBO(240, 249, 255, 1);
+  static const sky_100 = Color.fromRGBO(224, 242, 254, 1);
+  static const sky_200 = Color.fromRGBO(186, 230, 253, 1);
+  static const sky_300 = Color.fromRGBO(125, 211, 252, 1);
+  static const sky_400 = Color.fromRGBO(56, 189, 248, 1);
+  static const sky_500 = Color.fromRGBO(14, 165, 233, 1);
+  static const sky_600 = Color.fromRGBO(2, 132, 199, 1);
+  static const sky_700 = Color.fromRGBO(3, 105, 161, 1);
+  static const sky_800 = Color.fromRGBO(7, 89, 133, 1);
+  static const sky_900 = Color.fromRGBO(12, 74, 110, 1);
+  static const sky_950 = Color.fromRGBO(8, 47, 73, 1);
+
+  // blue
+  static const blue_50 = Color.fromRGBO(239, 246, 255, 1);
+  static const blue_100 = Color.fromRGBO(219, 234, 254, 1);
+  static const blue_200 = Color.fromRGBO(191, 219, 254, 1);
+  static const blue_300 = Color.fromRGBO(147, 197, 253, 1);
+  static const blue_400 = Color.fromRGBO(96, 165, 250, 1);
+  static const blue_500 = Color.fromRGBO(59, 130, 246, 1);
+  static const blue_600 = Color.fromRGBO(37, 99, 235, 1);
+  static const blue_700 = Color.fromRGBO(29, 78, 216, 1);
+  static const blue_800 = Color.fromRGBO(30, 64, 175, 1);
+  static const blue_900 = Color.fromRGBO(30, 58, 138, 1);
+  static const blue_950 = Color.fromRGBO(23, 37, 84, 1);
+
+  // indigo
+  static const indigo_50 = Color.fromRGBO(238, 242, 255, 1);
+  static const indigo_100 = Color.fromRGBO(224, 231, 255, 1);
+  static const indigo_200 = Color.fromRGBO(199, 210, 254, 1);
+  static const indigo_300 = Color.fromRGBO(165, 180, 252, 1);
+  static const indigo_400 = Color.fromRGBO(129, 140, 248, 1);
+  static const indigo_500 = Color.fromRGBO(99, 102, 241, 1);
+  static const indigo_600 = Color.fromRGBO(79, 70, 229, 1);
+  static const indigo_700 = Color.fromRGBO(67, 56, 202, 1);
+  static const indigo_800 = Color.fromRGBO(55, 48, 163, 1);
+  static const indigo_900 = Color.fromRGBO(49, 46, 129, 1);
+  static const indigo_950 = Color.fromRGBO(30, 27, 75, 1);
+
+  // violet
+  static const violet_50 = Color.fromRGBO(245, 243, 255, 1);
+  static const violet_100 = Color.fromRGBO(237, 233, 254, 1);
+  static const violet_200 = Color.fromRGBO(221, 214, 254, 1);
+  static const violet_300 = Color.fromRGBO(196, 181, 253, 1);
+  static const violet_400 = Color.fromRGBO(167, 139, 250, 1);
+  static const violet_500 = Color.fromRGBO(139, 92, 246, 1);
+  static const violet_600 = Color.fromRGBO(124, 58, 237, 1);
+  static const violet_700 = Color.fromRGBO(109, 40, 217, 1);
+  static const violet_800 = Color.fromRGBO(91, 33, 182, 1);
+  static const violet_900 = Color.fromRGBO(76, 29, 149, 1);
+  static const violet_950 = Color.fromRGBO(46, 16, 101, 1);
+
+  // purple
+  static const purple_50 = Color.fromRGBO(250, 245, 255, 1);
+  static const purple_100 = Color.fromRGBO(243, 232, 255, 1);
+  static const purple_200 = Color.fromRGBO(233, 213, 255, 1);
+  static const purple_300 = Color.fromRGBO(216, 180, 254, 1);
+  static const purple_400 = Color.fromRGBO(192, 132, 252, 1);
+  static const purple_500 = Color.fromRGBO(168, 85, 247, 1);
+  static const purple_600 = Color.fromRGBO(147, 51, 234, 1);
+  static const purple_700 = Color.fromRGBO(126, 34, 206, 1);
+  static const purple_800 = Color.fromRGBO(107, 33, 168, 1);
+  static const purple_900 = Color.fromRGBO(88, 28, 135, 1);
+  static const purple_950 = Color.fromRGBO(59, 7, 100, 1);
+
+  // fuchsia
+  static const fuchsia_50 = Color.fromRGBO(253, 244, 255, 1);
+  static const fuchsia_100 = Color.fromRGBO(250, 232, 255, 1);
+  static const fuchsia_200 = Color.fromRGBO(245, 208, 254, 1);
+  static const fuchsia_300 = Color.fromRGBO(240, 171, 252, 1);
+  static const fuchsia_400 = Color.fromRGBO(232, 121, 249, 1);
+  static const fuchsia_500 = Color.fromRGBO(217, 70, 239, 1);
+  static const fuchsia_600 = Color.fromRGBO(192, 38, 211, 1);
+  static const fuchsia_700 = Color.fromRGBO(162, 28, 175, 1);
+  static const fuchsia_800 = Color.fromRGBO(134, 25, 143, 1);
+  static const fuchsia_900 = Color.fromRGBO(112, 26, 117, 1);
+  static const fuchsia_950 = Color.fromRGBO(74, 4, 78, 1);
+
+  // pink
+  static const pink_50 = Color.fromRGBO(253, 242, 248, 1);
+  static const pink_100 = Color.fromRGBO(252, 231, 243, 1);
+  static const pink_200 = Color.fromRGBO(251, 207, 232, 1);
+  static const pink_300 = Color.fromRGBO(249, 168, 212, 1);
+  static const pink_400 = Color.fromRGBO(244, 114, 182, 1);
+  static const pink_500 = Color.fromRGBO(236, 72, 153, 1);
+  static const pink_600 = Color.fromRGBO(219, 39, 119, 1);
+  static const pink_700 = Color.fromRGBO(190, 24, 93, 1);
+  static const pink_800 = Color.fromRGBO(157, 23, 77, 1);
+  static const pink_900 = Color.fromRGBO(131, 24, 67, 1);
+  static const pink_950 = Color.fromRGBO(80, 7, 36, 1);
+
+  // rose
+  static const rose_50 = Color.fromRGBO(255, 241, 242, 1);
+  static const rose_100 = Color.fromRGBO(255, 228, 230, 1);
+  static const rose_200 = Color.fromRGBO(254, 205, 211, 1);
+  static const rose_300 = Color.fromRGBO(253, 164, 175, 1);
+  static const rose_400 = Color.fromRGBO(251, 113, 133, 1);
+  static const rose_500 = Color.fromRGBO(244, 63, 94, 1);
+  static const rose_600 = Color.fromRGBO(225, 29, 72, 1);
+  static const rose_700 = Color.fromRGBO(190, 18, 60, 1);
+  static const rose_800 = Color.fromRGBO(159, 18, 57, 1);
+  static const rose_900 = Color.fromRGBO(136, 19, 55, 1);
+  static const rose_950 = Color.fromRGBO(76, 5, 25, 1);
+}
